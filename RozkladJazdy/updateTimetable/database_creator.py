ef7f9cf9cae765a9747fe121164485fb4833438d
Marek Stereńczak <djvidix.ms@gmail.com>
2014-02-10 00:30:45 +0100

    small change by Łukasz

--- RozkladJazdy/updateTimetable/database_creator.py
+++ RozkladJazdy/updateTimetable/database_creator.py
@@ -17,9 +17,9 @@ def createDatabase(name):
     kursy = """CREATE TABLE Kursy (
                 Id INTEGER PRIMARY KEY,
                 Nr_linii INTEGER,
-                Godzina_odjazdu_DR INTEGER,
-                Godzina_odjazdu_S INTEGER,
-                Godzina_odjazdu_N INTEGER,
+                DR INTEGER,
+                S INTEGER,
+                N INTEGER,
                 Poczatkowy_przystanek INTEGER,
                 Koncowy_przystanek INTEGER,
                 FOREIGN KEY(Poczatkowy_przystanek) REFERENCES Przystanek(Id),
